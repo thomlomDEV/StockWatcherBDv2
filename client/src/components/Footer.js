4c9a7fe532039214879f7125fb28d90d71123eab
DEVX1 <38556505+DEVX1@users.noreply.github.com>
2018-04-30 10:58:19 +0200

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,10 +3,10 @@ import '../styles/footer.css';
 
 const Footer = () => (
   <div className="footer">
-    <h6 className="text-sm-center"><i className="fa fa-code"></i> by thomlom</h6>
+    <h6 className="text-sm-center"><i className="fa fa-code"></i> by Lukas Campbell</h6>
     <p className="text-sm-center">
-      <a className="social-link" href="https://github.com/thomlom" target="_blank" rel="noopener noreferrer"><i className="fa fa-2x fa-github"></i></a>
-      <a className="social-link" href="https://twitter.com/thomas_lombart" target="_blank" rel="noopener noreferrer"><i className="fa fa-2x fa-twitter"></i></a>
+      <a className="social-link" href="https://github.com/DEVX1" target="_blank" rel="noopener noreferrer"><i className="fa fa-2x fa-github"></i></a>
+      <a className="social-link" href="https://twitter.com" target="_blank" rel="noopener noreferrer"><i className="fa fa-2x fa-twitter"></i></a>
     </p>
   </div>
 )
